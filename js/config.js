4201389d6574107abfffe92522a8c5dd1ba1a122
praveen200011 <149142906+praveen200011@users.noreply.github.com>
2025-03-20 15:24:13 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_cFfjW954o', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '4hq89c0ulnvti05p095q5g7ijb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientSecret: '1h0it2ti7ou1s0ukn8ou840dugie0i8u3pqel0b1542kvmqoe4r5',
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
